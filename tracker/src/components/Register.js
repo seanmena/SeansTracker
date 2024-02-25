dd292cc1b7afaa4b58994c7edc684484a6c175de
Sean Mena <seanmena13@gmail.com>
2024-02-24 20:17:21 -0700

    created register page with input and useState

---
+++ tracker/src/components/Register.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  // set up my states for user and password
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="Register">
+      {/* create a form that uses usestate to grab a username and password */}
+      <div>
+        <h2>Register</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleRegister}>Register</button>
+      </div>
+      <div>
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
